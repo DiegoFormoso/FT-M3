ab2cb39fd28c0d5c57e24e6b84cff6f34f397340
DiegoFormoso <dformoso@yahoo.com>
2022-11-03 09:06:41 -0300

    hw 3

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,58 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+
+function replaceData(template, beatle) {
+  //var find = '{name}';
+  //var reg = new RegExp(find, 'g');
+  // return template.replace(reg, beatle.name)
+  //   .replace('{birth}', beatle.birthdate)
+  //   .replace('{profilePic}', beatle.profilePic);
+  return template.replace(/{name}/g, beatle.name)  // reemplaza en todos los names 
+                                                   //regular expresion --> regexpr.com
+    .replace(/{birth}/, beatle.birthdate)
+    .replace(/{profilePic}/, beatle.profilePic);
+}
+
+http.createServer(function(req, res) {
+  if (req.url === '/api' || req.url === '/api/') {
+    res.writeHead(200, {'Content-type': 'application/json'});
+    res.end(JSON.stringify(beatles));
+  }
+
+  if (req.url.substring(0, 5) === '/api/' && req.url.length > 5) {
+    let findBeatle = req.url.split('/').pop();
+    //let foundBeatle = beatles.filter(b => encodeURI(b.name) !== findBeatle);
+    let foundBeatle =  beatles.find(b => findBeatle === encodeURI(b.name));
+    if (foundBeatle) {
+       res.writeHead(200, {'Content-type': 'application/json'});
+       res.end(JSON.stringify(foundBeatle));
+    }
+    else {
+       res.writeHead(400, {'Content-type': 'text/plain'});
+       res.end('Beatle no encontrado.');
+    }
+  }
+
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    let html = fs.readFileSync(`${__dirname}/index.html`);
+    res.end(html);
+  }
+
+  let findBeatle = req.url.split('/').pop();
+  let foundBeatle =  beatles.find(b => findBeatle === encodeURI(b.name));
+  if (foundBeatle) {
+     res.writeHead(200, {'Content-type': 'text/html'});
+     let beatle = fs.readFileSync(`${__dirname}/beatle.html`, 'utf-8');
+     let finalBeatle = replaceData(beatle, foundBeatle);
+     res.end(finalBeatle);
+  }
+  else {
+     res.writeHead(400, {'Content-type': 'text/plain'});
+     res.end('Beatle no encontrado.');
+  }
+
+
+}).listen(3000, '127.0.0.1');
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,29 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer(function(req, res) {
+
+    // if (req.url === '/arcoiris_doge') {
+    //     fs.readFile(`${___dirnamme}/images/arcoiris_doge.jpg`, function(err, img){
+    //         if (err) {
+    //             res.writeHead(400, {'Content-type': 'text/plain'});
+    //             res.end('Hubo un error en la lectura')
+    //         }
+    //         else{
+    //             res.writeHead(200, {'Content-type': 'image/jpg'});
+    //             res.end(img);
+    //         }    
+    //     })
+    // }
+    fs.readFile(`${__dirname}/images${req.url}.jpg`, function(err, img) {
+            if (err) {
+                res.writeHead(400, {'Content-type': 'text/plain'});
+                res.end('Hubo un error en la lectura de la imagen')
+            }
+            else{
+                res.writeHead(200, {'Content-type': 'image/jpg'});
+                res.end(img);
+            }    
+    })    
+
+}).listen(3000, '127.0.0.1');
