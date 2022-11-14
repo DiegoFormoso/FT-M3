691739d7de72f1fa020cb5d48c3ff18fea5dab2e
DiegoFormoso <dformoso@yahoo.com>
2022-11-14 14:15:23 -0300

    hw 6

--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -16,7 +16,7 @@ describe('Test de APIS', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
     it('responds with and object with message `test`', () =>
       agent.get('/test').then((res) => {
-        expect(res.body.message).toEqual('hola');
+        expect(res.body.message).toEqual('test');
       }));
   });
 
@@ -43,14 +43,95 @@ describe('Test de APIS', () => {
   });
 
   describe('POST /sumArray', () => {
-    it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
+    it('responds with 200', () => 
+      agent
+        .post('/sumArray').expect(200)
+        .send({array: [2,5,7,10,11,15,20], num: 13})
+        .expect(200));
+
+    it('responds with an object with the result true if a combination of two numbers get the target', () =>
       agent.post('/sumArray')
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
           expect(res.body.result).toEqual(true);
       }));
+
+
+    it('responds with an object with the return false if does not exist a combination of two numbers', () => {
+        return agent
+          .post("/sumArray")
+          .send({array: [1,2,3,4,5], num: 100})
+          .then(res => {
+            expect(res.body.result).toEqual(false);
+        })
+    });
+
+    it('no puede sumar dos numeros iguales', () => {
+        return agent
+          .post("/sumArray")
+          .send({array: [1, 2, 3, 4], num: 2})
+          .then(res => {
+            expect(res.body.result).toEqual(false);
+          });
+      });
   });
 
-});
+  describe("POST /numString", ()=> {
+    it("responde con status 200", () => {
+      return agent.post("/numString").send({string: "hola"}).expect(200);
+    });
+    it("responde con 4 si enviamos hola", () => {
+      return agent
+          .post("/numString").send({string: "hola"})
+          .then(res => {
+             expect(res.body.result).toEqual(4);
+          })
+    });
+    it("responder con un status 400 (bad request) si el string es un número", () => {
+      return agent.post("/numString").send({string: 5}).expect(400);
+    });
+    it("responder con un status 400 (bad request) si el string esta vacio", () => {
+      return agent.post("/numString").send({string: ""}).expect(400);
+    });
+  });
 
+  describe("POST /pluck", ()=> {
+    it("responde con status 200", () => {
+      return agent.post("/pluck")
+      .send({
+        array: [
+          {nombre: "jorge", apellido:"pepe"}, 
+          {nombre: "diego", apellido: "formo"}
+        ], 
+        string: "nombre"}).expect(200);
+    });
+
+    it("responde con la funcionalidad de pluck", () => {
+      return agent.post("/pluck")
+      .send({
+        array: [
+          {nombre: "jorge", apellido:"pepe"}, 
+          {nombre: "diego", apellido: "formo"}
+        ], 
+        string: "nombre"})
+        .then((response) => {
+          expect(response.body.result).toEqual(["jorge", "diego"])
+        });
+    });
+
+    it("responder con un status 400 (bad request) si array no es un arreglo", () => {
+      return agent.post("/pluck")
+      .send({array: "holis", string: "nombre"}).expect(400);
+    });
+
+    it("responder con un status 400 (bad request) si el string propiedad está vacio", () => {
+      return agent.post("/pluck")
+      .send({
+        array: [
+          {nombre: "jorge", apellido:"pepe"}, 
+          {nombre: "diego", apellido: "formo"}
+        ], 
+        string: ""}).expect(400);
+    });
+  });
+});
