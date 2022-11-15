d0d138453bc8ef8a7a7a8a0cb9f8a43d6397405c
DiegoFormoso <dformoso@yahoo.com>
2022-11-15 10:28:08 -0300

    hw 7 generators

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,18 @@
-function fizzBuzzGenerator(max) {
-  // Tu código acá:
-
+function* fizzBuzzGenerator(max) {
+  let number = 1;
+  while (!max || number <= max) {
+    if (number % 3 === 0 && number % 5 === 0) { 
+       yield "Fizz Buzz";
+    }
+    else if (number % 3 === 0) {
+       yield "Fizz";
+    }
+    else if (number % 5 === 0)
+       yield "Buzz";
+    else
+       yield number;   
+    number ++;
+  }
 }
 
 module.exports = fizzBuzzGenerator;
